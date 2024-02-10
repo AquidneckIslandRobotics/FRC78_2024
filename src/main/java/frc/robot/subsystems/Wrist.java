87782cd9cf74e11422ca55c7f80ce694ad3786cf
Maddie Gomes <123703734+MaddieG56@users.noreply.github.com>
2024-02-10 11:59:48 -0500

    wrist PID

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -4,13 +4,23 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.InchesPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+
 import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,6 +29,24 @@ public class Wrist extends SubsystemBase {
 
   private CANSparkMax wristNeo;
   private AbsoluteEncoder encoder;
+
+  private final double kS = 0;
+  private final double kV = 2.1;
+  private final double kA = 0;
+  private final double kG = 0.27;
+
+  private final double kDt = 0.02;
+
+  private final Measure<Velocity<Distance>> manualSpeed = InchesPerSecond.of(1);
+
+  private final ArmFeedforward feedforwardWrist = new ArmFeedforward(kS, kV, kA, kG);
+  private final ProfiledPIDController profilePIDWrist =
+      new ProfiledPIDController(
+          0.03,
+          0,
+          0,
+          new TrapezoidProfile.Constraints(
+              InchesPerSecond.of(8), InchesPerSecond.per(Seconds).of(6)));
 
   /** Creates a new Wrist. */
   public Wrist(int WRIST_ID, float WRIST_HIGH_LIM, float WRIST_LOW_LIM) {
@@ -46,7 +74,17 @@ public class Wrist extends SubsystemBase {
   }
 
   public Command setToTarget(double target) {
-    return runOnce(() -> wristNeo.getPIDController().setReference(target, ControlType.kPosition));
+    return runOnce(() -> profilePIDWrist.setGoal(target))
+        .andThen(
+            run(
+                () -> {
+                  wristNeo.setVoltage(
+                      profilePIDWrist.calculate(Units.metersToInches(encoder.getPosition()))
+                          + feedforwardWrist.calculate(
+                              encoder.getPosition(),
+                              MetersPerSecond.of(profilePIDWrist.getSetpoint().velocity)
+                                  .in(InchesPerSecond)));
+                }));
   }
 
   @Override
